fb377b487e088c80103bc6a65c3fa8925e5dd6e7
Kitsune7X <ryuizanagi@proton.me>
2025-10-25 19:07:24 +0900

    feat: finish fundamental functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,26 +4,41 @@ const GRID_SIZE = 16;
 // Get the canvas element
 const canvas = document.querySelector("#canvas");
 
-// Spawn the div inside the canvas
-// canvas.appendChild(div);
-
-function createSingleDiv() {
+function createSingleDiv(row) {
   // Create a single div element
   const div = document.createElement("div");
   // Set height and width for the div
   div.classList.add("div-single");
 
-  return canvas.appendChild(div);
+  return row.appendChild(div);
 }
-
-// console.log(createSingleDiv());
 
 // Create a row
 function createRow(size) {
+  // Create Grid by using 2 dimenssional loop
   for (let i = 0; i < size; i++) {
-    createSingleDiv();
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < size; j++) {
+      createSingleDiv(row);
+    }
+    canvas.appendChild(row);
   }
 }
 
 createRow(GRID_SIZE);
-// console.log(1rem);
+console.log(canvas);
+
+const drawPoint = Array.from(document.querySelectorAll(".div-single"));
+console.log(drawPoint);
+console.log(drawPoint[0]);
+
+// Set up mouse hover event listener
+canvas.addEventListener("mouseover", (e) => {
+  console.log(e.target);
+  console.log(typeof e.target);
+  console.log(e.target.firstElementChild);
+  if (!e.target.firstElementChild) {
+    e.target.classList.add("black");
+  }
+});

f12f6ad6dea34564aa22114916cdb40f4260868d
Kitsune7X <ryuizanagi@proton.me>
2025-10-26 00:46:39 +0900

    feat: complete app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,73 @@
-// Max Grid
-const GRID_SIZE = 16;
+// -------------------------------
+// Variable Declaration
+// -------------------------------
+
+// Default Cells
+const CELLS = 16;
 
 // Get the canvas element
 const canvas = document.querySelector("#canvas");
 
+// Get the cell option element
+const option = document.querySelector("#option");
+// Get the input element
+const input = document.querySelector("#custom-cell");
+
+// 1 Rem in Pixel
+const oneRemInPixel = parseFloat(
+  getComputedStyle(document.documentElement).fontSize
+);
+
+// -------------------------------
+// Main
+// -------------------------------
+
+// Create the default Grid
+createGrid(CELLS);
+
+// Dot drawing
+canvas.addEventListener("mousedown", drawDot);
+
+// Custom Cell
+option.addEventListener("click", (e) => {
+  e.preventDefault();
+  input.setAttribute("placeholder", "Number of Cells");
+
+  // Prevent action when clicking on the input field
+  if (e.target.id === "custom-cell") {
+    return;
+  }
+
+  // Generate Grid when user click on "Generate Grid button"
+  // Check for valid input
+  if (
+    e.target.id === "btnGenerateGrid" &&
+    (!Number.isInteger(parseInt(input.value)) || parseInt(input.value) <= 0)
+  ) {
+    input.value = "";
+    input.setAttribute("placeholder", "Number only");
+    return;
+  }
+  // If input is valid, assign input value to a variable for later calculation
+  const cellCount = input.value;
+
+  // Clear the input field
+  input.value = "";
+
+  createCustomGrid(cellCount);
+
+  // Reset the Grid
+  if (e.target.id === "btnReset") {
+    createGrid(CELLS);
+    return;
+  }
+});
+
+// -------------------------------
+// Functions
+// -------------------------------
+
+// Create a single div function
 function createSingleDiv(row) {
   // Create a single div element
   const div = document.createElement("div");
@@ -13,35 +77,53 @@ function createSingleDiv(row) {
   return row.appendChild(div);
 }
 
-// Create a row
-function createRow(size) {
+// Create Grid Function
+function createGrid(cell) {
   // Create Grid by using 2 dimenssional loop
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < cell; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
-    for (let j = 0; j < size; j++) {
+    for (let j = 0; j < cell; j++) {
       createSingleDiv(row);
     }
     canvas.appendChild(row);
   }
 }
 
-createRow(GRID_SIZE);
-console.log(canvas);
+// Drawn Dot function
+function drawDot(e) {
+  // Disable dragging element
+  e.preventDefault();
+  const dot = e.target;
+  // Since we listen on the canvas, we need to make sure only to draw dot
+  // on the single div, which mean the inner most child.
+  if (!dot.firstElementChild) {
+    dot.classList.add("black");
 
-const drawPoint = Array.from(document.querySelectorAll(".div-single"));
-console.log(drawPoint);
-console.log(drawPoint[0]);
-
-// Set up mouse hover event listener
-canvas.addEventListener("mousedown", (e) => {
-  // console.log(e.target.firstElementChild);
-  if (!e.target.firstElementChild) {
-    e.target.classList.add("black");
-    canvas.addEventListener("mouseover", (e) => {
-      if (!e.target.firstElementChild) {
-        e.target.classList.add("black");
-      }
-    });
+    canvas.addEventListener("mouseover", drawDot);
   }
-});
+  // Stop drawing dot when user release the mouse
+  canvas.addEventListener("mouseup", (e) => {
+    canvas.removeEventListener("mouseover", drawDot);
+  });
+}
+
+// Create Custom Grid
+function createCustomGrid(cellCount) {
+  // Calculate Cell Dimenstion in rem
+  const cellDimension = canvas.clientHeight / cellCount / oneRemInPixel;
+
+  // Select all row
+  const rowArr = Array.from(document.querySelectorAll(".row"));
+
+  rowArr.forEach((item) => item.remove());
+
+  // Create new Custom grid
+  createGrid(cellCount);
+
+  const divArr = Array.from(document.querySelectorAll(".div-single"));
+
+  divArr.forEach((item) => {
+    item.setAttribute("style", `width: ${cellDimension}rem`);
+  });
+}
